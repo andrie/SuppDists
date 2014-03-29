0746e0231a2ef4529c879cab9ed33b6c19fced18
Andrie <apdevries@gmail.com>
2014-03-29 10:12:16 +0000

    Added package documentation

--- R/SuppDists-package.R
+++ R/SuppDists-package.R
@@ -4,23 +4,23 @@
 #' The package includes ten distributions supplementing those built into \R:
 #' 
 #' \describe{ 
-#' \item{\code{invGauss}}{The inverse Gaussian and Wald distributions}
-#' \item{\code{KruskalWallis}}{Kruskall-Wallis distribution}
-#' \item{\code{Kendall}}{Kendall's Tau}
-#' \item{\code{Friedman}}{Friedman's chi squared}
-#' \item{\code{Spearman}}{Spearman's rho}
-#' \item{\code{maxFratio}}{The maximum F-ratio distribution}
-#' \item{\code{Pearson}}{Pearson product moment correlation coefficient}
-#' \item{\code{Johnson}}{Johnson distributions}
-#' \item{\code{NormalScore}}{normal scores}
-#' \item{\code{ghyper}}{Generalized hypergeometric distributions (Kemp and Kemp)}
+#' \item{\code{\link{dinvGauss}}}{The inverse Gaussian and Wald distributions}
+#' \item{\code{\link{dKruskalWallis}}}{Kruskall-Wallis distribution}
+#' \item{\code{\link{dKendall}}}{Kendall's Tau}
+#' \item{\code{\link{dFriedman}}}{Friedman's chi squared}
+#' \item{\code{\link{dSpearman}}}{Spearman's rho}
+#' \item{\code{\link{dmaxFratio}}}{The maximum F-ratio distribution}
+#' \item{\code{\link{dPearson}}}{Pearson product moment correlation coefficient}
+#' \item{\code{\link{dJohnson}}}{Johnson distributions}
+#' \item{\code{\link{dNormScore}}}{normal scores}
+#' \item{\code{\link{dghyper}}}{Generalized hypergeometric distributions (Kemp and Kemp)}
 #' }
 #' 
 #' In addition, two random number generators of George Marsaglia:
 #' 
 #' \describe{
-#' \item{\code{MWC1019}}{A very long period pseudo-random generator}
-#' \item{\code{ziggurat}}{The Ziggurat normal and exponential generator}
+#' \item{\code{\link{rMWC1019}}}{A very long period pseudo-random generator}
+#' \item{\code{\link{rziggurat}}}{The Ziggurat normal and exponential generator}
 #' }
 #' 
 #' @name SuppDists-package
